295d1b37c190f5f5e167ecb07361f793457b6a98
alessi1 <alessi1@lsu.edu>
2014-09-08 13:38:17 -0500

    Outline

--- LCD/LCD.ino
+++ LCD/LCD.ino
@@ -1,2 +1,23 @@
-//First commit
-//Alyssa!
+//Third commit
+//Alyssa
+void setup(){
+  //function LCD_Init
+LCD_Init;
+}
+void loop(){
+ LCD_Clear;
+ LCD_Display;
+ // stop program
+}
+
+
+
+void LCD_Init(){
+ }
+void LCD_Display(){
+  }
+void LCD_Clear(){
+  }
+
+
+
